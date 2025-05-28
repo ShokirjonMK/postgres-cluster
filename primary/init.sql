faa5561c3cb2a4e0d7565091629f158077a873e4
ShokirjonMK <mkshokirjon@gmail.com>
2025-05-28 09:17:12 +0500

    a

--- primary/init.sql
+++ primary/init.sql
@@ -1,6 +1,8 @@
--- Replication uchun sozlamalar
+-- Replication user
+CREATE ROLE replica_user WITH REPLICATION LOGIN PASSWORD 'replica_pass';
+
+-- Replication config
 ALTER SYSTEM SET wal_level = 'replica';
 ALTER SYSTEM SET max_wal_senders = 10;
 ALTER SYSTEM SET wal_keep_size = '64MB';
 ALTER SYSTEM SET hot_standby = 'on';
-SELECT pg_reload_conf();
