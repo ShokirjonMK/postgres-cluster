bcec48d3a6ec75a8f7dec199364ed743fe89cf97
ShokirjonMK <mkshokirjon@gmail.com>
2025-05-28 08:57:04 +0500

    Update init.sql

--- primary/init.sql
+++ primary/init.sql
@@ -1,5 +1,6 @@
--- Enable replication
-ALTER SYSTEM SET wal_level = replica;
+-- Replication uchun sozlamalar
+ALTER SYSTEM SET wal_level = 'replica';
 ALTER SYSTEM SET max_wal_senders = 10;
 ALTER SYSTEM SET wal_keep_size = '64MB';
+ALTER SYSTEM SET hot_standby = 'on';
 SELECT pg_reload_conf();
