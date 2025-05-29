4b2d15988d0542a41a9d14686f27f9e7b39696d4
ShokirjonMK <mkshokirjon@gmail.com>
2025-05-29 10:54:31 +0500

    1

--- primary/init.sql
+++ primary/init.sql
@@ -1,7 +1,7 @@
--- Replication user
+-- REPLICATION USER
 CREATE ROLE replica_user WITH REPLICATION LOGIN PASSWORD 'replica_pass';
 
--- Replication config
+-- REPLICATION SETTINGS
 ALTER SYSTEM SET wal_level = 'replica';
 ALTER SYSTEM SET max_wal_senders = 10;
 ALTER SYSTEM SET wal_keep_size = '64MB';
